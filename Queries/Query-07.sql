e46ca8b8a55981c393036dca6b7fa46db89f545e
Aliaksandr Scepanenka <alexander.stepanenko@intexsoft.by>
2020-02-21 13:48:02 +0300

    feat: implement Query-07

--- Queries/Query-07.sql
+++ Queries/Query-07.sql
@@ -1,10 +1,13 @@
-/* Вычислить число продаж, их общую сумму, средневзвешенную,
- максимальную и минимальную скидки по каждому клиенту,
- у которых были продажи со скидкой более, чем 25%.
- Все — без учета скидок на строки */
+/* Вычислить число продаж, их общую сумму, средневзвешенную, максимальную и минимальную скидки по каждому клиенту,
+ у которых были продажи со скидкой более, чем 25%. Все — без учета скидок на строки */
 
-SELECT cl.MONIKER, count(s.ID_SALE), SUM(s.SUMMA)
+SELECT cl.moniker AS client,
+       count(s.id_sale) sales_cnt,
+       SUM(s.summa) AS sales_summa,
+       SUM(s.summa)/count(s.id_sale) as avg_sale_sum,
+       MAX(s.discount) AS max_discount,
+       MIN(s.discount) AS min_discount
 FROM T_SALE s
-JOIN T_CLIENT cl USING (ID_CLIENT)
+         JOIN T_CLIENT cl ON (s.ID_CLIENT = cl.ID_CLIENT)
 GROUP BY cl.MONIKER
-ORDER BY cl.MONIKER
+HAVING MAX(s.discount) > 25
