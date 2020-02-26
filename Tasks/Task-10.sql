a35bed4f805b63dfa039ab19f94bf5c27e1f99ab
Aliaksandr Scepanenka <alexander.stepanenko@intexsoft.by>
2020-02-26 11:26:59 +0300

    feat: implement procedure node_move from Task-10

--- Tasks/Task-10.sql
+++ Tasks/Task-10.sql
@@ -8,13 +8,27 @@ CREATE OR REPLACE PROCEDURE node_add(i_id_parent t_ctl_node.id_parent%TYPE,
     IS
     l_tree_code t_ctl_node.tree_code%TYPE;
 BEGIN
-    SELECT tree_code
-    INTO l_tree_code
-    FROM t_ctl_node
-    WHERE ID_CTL_NODE = i_id_parent;
+    SELECT tree_code INTO l_tree_code FROM t_ctl_node WHERE id_ctl_node = i_id_parent;
     l_tree_code := NVL(l_tree_code, '') || '/' || i_code;
     INSERT INTO t_ctl_node (id_parent, code, tree_code, name) VALUES (i_id_parent, i_code, l_tree_code, i_name);
     COMMIT;
 END node_add;
+/
 
 -- Перемещение
+CREATE OR REPLACE PROCEDURE node_move (
+    i_id_node t_ctl_node.id_ctl_node%TYPE,
+    i_id_new_parent t_ctl_node.id_parent%TYPE
+)
+IS
+    l_tree_code t_ctl_node.tree_code%TYPE;
+    l_code t_ctl_node.code%TYPE;
+BEGIN
+    SELECT tree_code INTO l_tree_code FROM t_ctl_node WHERE id_ctl_node = i_id_new_parent;
+    SELECT code INTO l_code FROM t_ctl_node WHERE id_ctl_node = i_id_node;
+    l_tree_code := NVL(l_tree_code, '') || '/' || l_code;
+    UPDATE t_ctl_node SET id_parent = i_id_new_parent,
+                            tree_code = l_tree_code
+    WHERE id_ctl_node = i_id_node;
+END node_move;
+/
